120f0aa599fe11816d348824c77b3e0f2b149ed8
Sushank Sakthivel Nagarajan <60143563+Sushank34@users.noreply.github.com>
2021-07-19 10:32:44 +0530

    Create Find the Runner-Up Score!.py

---
+++ Codes/Python/Find the Runner-Up Score!.py
@@ -0,0 +1,17 @@
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int, input().split())
+
+    listnew=[]
+
+    for i in arr:
+
+         if i not in listnew:
+
+
+                    listnew.append(i)
+
+    listnew.sort()
+    listnew.reverse()
+
+    print(listnew[1])
